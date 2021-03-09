715d4decc4f1a1a042efe53d6d6554f21388d8a8
requelme07 <ilabaz210191@gmail.com>
2021-03-09 17:23:03 +0300

    lesson47

--- social-network/src/index.js
+++ social-network/src/index.js
@@ -5,21 +5,17 @@ import store from "./redux/redux-store";
 import "./index.css";
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
-export let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} dispatch={store.dispatch.bind(store)} store={store}/>
+            <Provider store={store}>
+            <App />
+            </Provider>
         </BrowserRouter>,
         document.getElementById("root")
     );
-};
 
-rerenderEntireTree(store.getState());
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
