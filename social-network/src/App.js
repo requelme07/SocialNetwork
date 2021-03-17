9bd4a380e5fbf958dd4798919e37f2461ec6bccf
Iya Labadze <ilabaz210191@gmail.com>
2021-03-17 11:34:06 +0700

    rebranding

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import {BrowserRouter, Route} from "react-router-dom";
-
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
